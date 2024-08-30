d0daf2a8bfafaf876fc066a05769ac3edb5d45f0
KomoriDev <mute231010@gmail.com>
2024-08-30 20:08:35 +0800

    :tada: first commit

---
+++ nonebot_plugin_telegram_games/__init__.py
@@ -0,0 +1,78 @@
+from nonebot import on, require
+from nonebot.adapters.telegram import Bot
+from nonebot.plugin import PluginMetadata
+from nonebot.adapters.telegram.event import InlineQueryEvent, CallbackQueryEvent
+from nonebot.adapters.telegram.model import (
+    CallbackGame,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultGame,
+)
+
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import Button, Command, UniMessage, CommandMeta
+
+__plugin_meta__ = PluginMetadata(
+    name="Telegram Games",
+    description="Telegram Games",
+    usage="/game",
+    type="application",
+    config=None,
+    homepage="https://github.com/KomoriDev/nonebot-plugin-telegram-games",
+    supported_adapters={"~telegram"},
+    extra={
+        "unique_name": "Telegram Games",
+        "author": "Komorebi <mute231010@gmail.com>",
+        "version": "0.1.0",
+    },
+)
+
+
+game = Command("game", meta=CommandMeta(description="Telegram 游戏")).build(
+    use_cmd_start=True
+)
+
+
+@game.handle()
+async def _():
+    await (
+        UniMessage.text("I can get you 1 fun games to play. ")
+        .text("Just tap 'Play', then select a game.")
+        .keyboard(Button("input", label="Play", text="/"))
+        .finish()
+    )
+
+
+@on("inline").handle()
+async def _(bot: Bot, event: InlineQueryEvent):
+    await bot.answer_inline_query(
+        inline_query_id=event.id,
+        results=[
+            InlineQueryResultGame(
+                id="game_2048",
+                game_short_name="game2048",
+                reply_markup=InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [
+                            InlineKeyboardButton(
+                                text="Play 2048",
+                                callback_game=CallbackGame(),
+                            )
+                        ],
+                        [
+                            InlineKeyboardButton(
+                                text="Star",
+                                url="https://github.com/KomoriDev/nonebot-plugin-telegram-games",
+                            )
+                        ],
+                    ]
+                ),
+            )
+        ],
+    )
+
+
+@on("inline").handle()
+async def _(bot: Bot, event: CallbackQueryEvent):
+    if event.game_short_name:
+        await bot.answer_callback_query(event.id, text=f"{event.game_short_name} 启动")
---
+++ nonebot_plugin_telegram_games/apps/2048/app.js
@@ -0,0 +1,164 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let score = 0;
+  const boardSize = 4;
+
+  let board = Array(boardSize * boardSize).fill(0);
+
+  const tiles = document.querySelectorAll('.tile');
+  const scoreDisplay = document.querySelector('#score')
+
+  function initBoard() {
+    generateTile();
+    generateTile();
+    updateBoard();
+  }
+
+  function updateBoard() {
+    tiles.forEach((tile, index) => {
+      tile.textContent = board[index] === 0 ? '' : board[index];
+      tile.setAttribute('data-board', board[index]);
+    });
+  }
+
+  function generateTile() {
+    let emptyTiles = board
+      .map((val, idx) => (val === 0 ? idx : null))
+      .filter(val => val !== null);
+
+    if (emptyTiles.length === 0) return;
+
+    let randomIndex = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+    let newValue = Math.random() > 0.1 ? 2 : 4;
+    board[randomIndex] = newValue;
+
+    const tileElement = tiles[randomIndex];
+    tileElement.textContent = newValue;
+    tileElement.classList.add('new');
+
+    tileElement.addEventListener('animationend', () => {
+        tileElement.classList.remove('new');
+    });
+  }
+
+
+  function slide(row) {
+    row = row.filter(val => val);
+    while (row.length < boardSize) {
+      row.push(0);
+    }
+    return row;
+  }
+
+  function combine(row) {
+    for (let i = 0; i < row.length - 1; i++) {
+      if (row[i] === row[i + 1] && row[i] !== 0) {
+        row[i] *= 2;
+        row[i + 1] = 0;
+        score += row[i];
+        scoreDisplay.textContent = score;
+      }
+    }
+    return row;
+  }
+
+  function handleMove() {
+
+    setTimeout(() => {
+      generateTile();
+      updateBoard();
+    }, 130);
+
+    function isGameOver() {
+      if (board.includes(0)) return false;
+      for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize - 1; j++) {
+          if (board[i * boardSize + j] === board[i * boardSize + j + 1]) return false;
+          if (board[j * boardSize + i] === board[(j + 1) * boardSize + i]) return false;
+        }
+      }
+      return true;
+    }
+
+    if (isGameOver()) {
+      window.location.href = `over.html?score=${score}`;
+    }
+  }
+
+  function moveLeft() {
+    for (let i = 0; i < boardSize; i++) {
+      let row = board.slice(i * boardSize, i * boardSize + boardSize);
+      row = slide(row);
+      row = combine(row);
+      row = slide(row);
+      board.splice(i * boardSize, boardSize, ...row);
+    }
+    handleMove();
+  }
+
+  function moveRight() {
+    for (let i = 0; i < boardSize; i++) {
+      let row = board.slice(i * boardSize, i * boardSize + boardSize);
+      row.reverse();
+      row = slide(row);
+      row = combine(row);
+      row = slide(row);
+      row.reverse();
+      board.splice(i * boardSize, boardSize, ...row);
+    }
+    handleMove();
+  }
+
+  function moveUp() {
+    for (let i = 0; i < boardSize; i++) {
+      let column = [];
+      for (let j = 0; j < boardSize; j++) {
+        column.push(board[i + j * boardSize]);
+      }
+      column = slide(column);
+      column = combine(column);
+      column = slide(column);
+      for (let j = 0; j < boardSize; j++) {
+        board[i + j * boardSize] = column[j];
+      }
+    }
+    handleMove();
+  }
+
+  function moveDown() {
+    for (let i = 0; i < boardSize; i++) {
+      let column = [];
+      for (let j = 0; j < boardSize; j++) {
+        column.push(board[i + j * boardSize]);
+      }
+      column.reverse();
+      column = slide(column);
+      column = combine(column);
+      column = slide(column);
+      column.reverse();
+      for (let j = 0; j < boardSize; j++) {
+        board[i + j * boardSize] = column[j];
+      }
+    }
+    handleMove();
+  }
+
+
+  document.addEventListener('keyup', (e) => {
+    switch (e.key) {
+      case 'ArrowLeft':
+        moveLeft();
+        break;
+      case 'ArrowRight':
+        moveRight();
+        break;
+      case 'ArrowUp':
+        moveUp();
+        break;
+      case 'ArrowDown':
+        moveDown();
+        break;
+    }
+  });
+
+  initBoard();
+});
